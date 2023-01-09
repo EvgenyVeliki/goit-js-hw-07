ecfa306a04a6f1551649ef6de696c0d6fe175c04
EvgenyVeliki <108056667+EvgenyVeliki@users.noreply.github.com>
2023-01-09 17:17:52 +0200

    HomeWork 7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+
+
+const boxGalleryItems = document.querySelector('.gallery')
+
+const listItemsMarkup = createListItemMarkup(galleryItems)
+
+
+boxGalleryItems.innerHTML = listItemsMarkup
+
+
+
+function createListItemMarkup (items) {
+    return items.map(item => `<div class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+      <img
+        class="gallery__image"
+        src="${item.preview}"
+        data-source="${item.original}"
+        alt="${item.description}"
+      />
+    </a>
+  </div>`).join('')
+}
+
+boxGalleryItems.addEventListener('click', onImageClick)
+
+
+function onImageClick(evt) {
+    evt.preventDefault()
+if (evt.target.nodeName !== 'IMG') {
+    return
+}
+
+const instance = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}" width="800" height="600">
+`)
+
+instance.show()
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,34 @@
 import { galleryItems } from './gallery-items.js';
+
 // Change code below this line
 
+
 console.log(galleryItems);
+const boxGalleryItems = document.querySelector('.gallery')
+
+const listItemsMarkup = createListItemMarkup(galleryItems)
+
+
+boxGalleryItems.innerHTML = listItemsMarkup
+
+
+
+function createListItemMarkup (items) {
+    return items.map(item => `<a class="gallery__item" href="${item.original}">
+    <img class="gallery__image" src="${item.preview}" alt="${item.description}" data-caption="${item.description}" />
+  </a>`).join('')
+}
+
+boxGalleryItems.addEventListener('click', onImageClick)
+
+
+function onImageClick(evt) {
+    evt.preventDefault()
+if (evt.target.nodeName !== 'IMG') {
+    return
+}
+
+
+const lightbox = new SimpleLightbox('.gallery a', {captionsData: "alt",
+captionDelay: 250,});}
+
